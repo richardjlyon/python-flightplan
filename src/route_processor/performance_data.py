f2654725166eaf2605ca97cb8d4946926289fc10
Richard Lyon <6917342+richardjlyon@users.noreply.github.com>
2025-02-22 15:59:23 +0000

    Impplement cruise performance data

--- src/route_processor/performance_data.py
+++ src/route_processor/performance_data.py
@@ -11,13 +11,15 @@ import pandas as pd
 
 
 class JetOperation(Enum):
-    """Enum to represent climb and descent operations."""
+    """Enum to represent climb, descent, and cruise operations."""
 
-    NORMALCLIMB = "normal_climb"
-    NAVDESCENT = "nav_descent"
+    NORMAL_CLIMB = "normal_climb"
+    NAV_DESCENT = "nav_descent"
+    LL_CRUISE = "ll_cruise"
+    ML_CRUISE = "ml_cruise"
 
 
-class PerformanceData(BaseModel):
+class ClimbDescentPerformanceData(BaseModel):
     """
     Represents performance data with validation.
     Includes distance, time (in seconds), and fuel in kilograms.
@@ -44,30 +46,49 @@ class PerformanceData(BaseModel):
         )
 
 
-def get_performance_data(operation: JetOperation, flight_level: int) -> PerformanceData:
-    # Define the data file path based on the operation
-    current_file_directory = Path(__file__).parent
-    file_name = f"{operation.value.lower()}_performance.csv"
-    file_path = current_file_directory / "performance_data" / file_name
+class LLCruisePerformanceData(BaseModel):
+    """
+    Represents low level cruise performance data.
+    """
 
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"Performance data file not found: {file_path}")
+    kg_min: float = Field(
+        ..., ge=0, description="Fuel consumption kg/min; must be at least 0."
+    )
+    operation: JetOperation = Field(
+        ..., description="The type of operation, either 'CLIMB' or 'DESCENT'."
+    )
 
-    # Load the CSV data
-    try:
-        df = pd.read_csv(file_path)
-    except Exception as e:
-        raise ValueError(f"Error reading CSV file: {e}")
+
+class MLCruisePerformanceData(BaseModel):
+    """
+    Represents medium level cruise performance data.
+    """
+
+    kg_min: float = Field(
+        ..., ge=0, description="Fuel consumption kg/min; must be at least 0."
+    )
+    kg_anm: float = Field(
+        ..., ge=0, description="Fuel consumption kg/anm; must be at least 0."
+    )
+    operation: JetOperation = Field(
+        ..., description="The type of operation, either 'CLIMB' or 'DESCENT'."
+    )
+
+
+def get_climb_descent_performance_data(
+        operation: JetOperation, flight_level: int
+) -> ClimbDescentPerformanceData:
+    df = load_df(operation)
 
     # Prepare the dataframe
     df["time_secs"] = df["time"].apply(mmss_to_seconds)
     df = df.drop(columns=["time"])
     df = df.sort_values(by="fl").reset_index(drop=True)
 
-    result = lookup(df, flight_level)
+    result = lookup_fl(df, flight_level)
 
     # Create and return a PerformanceData object
-    return PerformanceData(
+    return ClimbDescentPerformanceData(
         distance_nm=result["distance_nm"],
         time_secs=result["time_secs"],
         fuel_kg=result["fuel_kg"],
@@ -75,17 +96,76 @@ def get_performance_data(operation: JetOperation, flight_level: int) -> Performa
     )
 
 
-def lookup(df, flight_level):
+def get_ll_cruise_performance_data(
+        operation: JetOperation, speed_kts: int
+) -> LLCruisePerformanceData:
+    df = load_df(operation)
+    df = df.sort_values(by="kts").reset_index(drop=True)
+    result = lookup_kts(df, speed_kts)
+
+    return LLCruisePerformanceData(
+        kg_min=result["kg_min"],
+        operation=operation,
+    )
+
+
+def get_ml_cruise_performance_data(
+        operation: JetOperation, flight_level: int
+) -> MLCruisePerformanceData:
+    df = load_df(operation)
+    df = df.sort_values(by="fl").reset_index(drop=True)
+    result = lookup_fl(df, flight_level)
+
+    return MLCruisePerformanceData(
+        kg_min=round(result["kg_min"], 2),
+        kg_anm=round(result["kg_anm"], 2),
+        operation=operation,
+    )
+
+
+def load_df(operation):
+    current_file_directory = Path(__file__).parent
+    file_name = f"{operation.value.lower()}_performance.csv"
+    file_path = current_file_directory / "performance_data" / file_name
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Performance data file not found: {file_path}")
+    # Load the CSV data
+    try:
+        df = pd.read_csv(file_path)
+    except Exception as e:
+        raise ValueError(f"Error reading CSV file: {e}")
+    return df
+
+
+def lookup_fl(df: pd.DataFrame, flight_level: int):
     if flight_level not in df["fl"].values:
         # Interpolate values for the missing flight level
         df = (
             df.set_index("fl").reindex(df["fl"].tolist() + [flight_level]).sort_index()
         )  # Add and sort index with the new flight level
         df = df.interpolate(method="linear", limit_direction="both").reset_index()
+
     # Extract the row for the requested flight level
     result_row = df.loc[df["fl"] == flight_level]
     if result_row.empty:
         raise ValueError(f"Unable to interpolate data for flight level {flight_level}")
+
+    return result_row
+
+
+def lookup_kts(df: pd.DataFrame, speed_kts: int):
+    if speed_kts not in df["kts"].values:
+        # Interpolate values for the missing airspeed
+        df = (
+            df.set_index("kts").reindex(df["kts"].tolist() + [speed_kts]).sort_index()
+        )  # Add and sort index with the new flight level
+        df = df.interpolate(method="linear", limit_direction="both").reset_index()
+
+    # Extract the row for the requested flight level
+    result_row = df.loc[df["kts"] == speed_kts]
+    if result_row.empty:
+        raise ValueError(f"Unable to interpolate data for flight level {speed_kts}")
+
     return result_row
 
 
--- tests/route_processor/test_performance_data.py
+++ tests/route_processor/test_performance_data.py
@@ -1,9 +1,13 @@
 import pytest
 
 from src.route_processor.performance_data import (
-    get_performance_data,
+    get_climb_descent_performance_data,
     JetOperation,
-    PerformanceData,
+    ClimbDescentPerformanceData,
+    get_ll_cruise_performance_data,
+    LLCruisePerformanceData,
+    get_ml_cruise_performance_data,
+    MLCruisePerformanceData,
 )
 
 
@@ -11,47 +15,109 @@ from src.route_processor.performance_data import (
     "operation, fl, expected",
     [
         (
-                JetOperation.NORMALCLIMB,
-                200,  # No interpolation
-                PerformanceData(
-                    distance_nm=26.0,
-                    time_secs=228,
-                    fuel_kg=86.0,
-                    operation=JetOperation.NORMALCLIMB,
-                ),
+            JetOperation.NORMAL_CLIMB,
+            200,  # No interpolation
+            ClimbDescentPerformanceData(
+                distance_nm=26.0,
+                time_secs=228,
+                fuel_kg=86.0,
+                operation=JetOperation.NORMAL_CLIMB,
+            ),
         ),
         (
-                JetOperation.NORMALCLIMB,
-                220,  # Interpolation
-                PerformanceData(
-                    distance_nm=30.0,
-                    time_secs=264,
-                    fuel_kg=96.0,
-                    operation=JetOperation.NORMALCLIMB,
-                ),
+            JetOperation.NORMAL_CLIMB,
+            220,  # Interpolation
+            ClimbDescentPerformanceData(
+                distance_nm=30.0,
+                time_secs=264,
+                fuel_kg=96.0,
+                operation=JetOperation.NORMAL_CLIMB,
+            ),
         ),
         (
-                JetOperation.NAVDESCENT,
-                250,  # No interpolation
-                PerformanceData(
-                    distance_nm=25.0,
-                    time_secs=210,
-                    fuel_kg=11.0,
-                    operation=JetOperation.NAVDESCENT,
-                ),
+            JetOperation.NAV_DESCENT,
+            250,  # No interpolation
+            ClimbDescentPerformanceData(
+                distance_nm=25.0,
+                time_secs=210,
+                fuel_kg=11.0,
+                operation=JetOperation.NAV_DESCENT,
+            ),
         ),
         (
-                JetOperation.NAVDESCENT,
-                325,  # Interpolation
-                PerformanceData(
-                    distance_nm=33.5,
-                    time_secs=276,
-                    fuel_kg=14.0,
-                    operation=JetOperation.NAVDESCENT,
-                ),
+            JetOperation.NAV_DESCENT,
+            325,  # Interpolation
+            ClimbDescentPerformanceData(
+                distance_nm=33.5,
+                time_secs=276,
+                fuel_kg=14.0,
+                operation=JetOperation.NAV_DESCENT,
+            ),
         ),
     ],
 )
-def test_normal_climb(operation, fl, expected):
-    result = get_performance_data(operation, fl)
+def test_climb_descent(operation, fl, expected):
+    result = get_climb_descent_performance_data(operation, fl)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "operation, kts, expected",
+    [
+        (
+            JetOperation.LL_CRUISE,
+            420,
+            LLCruisePerformanceData(
+                kg_min=18.9,
+                operation=JetOperation.LL_CRUISE,
+            ),
+        ),
+        (
+            JetOperation.LL_CRUISE,
+            330,
+            LLCruisePerformanceData(
+                kg_min=12.1,
+                operation=JetOperation.LL_CRUISE,
+            ),
+        ),
+        (
+            JetOperation.LL_CRUISE,
+            300,
+            LLCruisePerformanceData(
+                kg_min=10.5,
+                operation=JetOperation.LL_CRUISE,
+            ),
+        ),
+    ],
+)
+def test_ll_cruise(operation, kts, expected):
+    result = get_ll_cruise_performance_data(operation, kts)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "operation, fl, expected",
+    [
+        (
+            JetOperation.ML_CRUISE,
+            200,
+            MLCruisePerformanceData(
+                kg_min=12.2,
+                kg_anm=1.59,
+                operation=JetOperation.ML_CRUISE,
+            ),
+        ),
+        (
+            JetOperation.ML_CRUISE,
+            325,
+            MLCruisePerformanceData(
+                kg_min=7.55,
+                kg_anm=1.03,
+                operation=JetOperation.ML_CRUISE,
+            ),
+        ),
+    ],
+)
+def test_ml_cruise(operation, fl, expected):
+    result = get_ml_cruise_performance_data(operation, fl)
     assert result == expected
